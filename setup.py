1c123dc05800f8615f184d4339a2813c760135c8
urushiyama <aswif10flis1ntkb@gmail.com>
2020-10-26 22:15:01 +0900

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PySegmentKit",
-    version="0.1.1",
+    version="0.2.0",
     author="Yuta Urushiyama",
     author_email="aswif10flis1ntkb@gmail.com",
     description="A python-port of julius-speech/segmentation-kit",
